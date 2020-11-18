d5092dc686095a0ec6395843dead5c0fb0433f22
tsimafei-sakalou <tsimafei.sakalou@instinctools.com>
2020-11-18 16:12:13 +0300

    Create register/login form tabs

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Tab {
+  title: string;
+  isActive: boolean;
+}
 
 @Component({
   selector: 'app-register',
@@ -8,14 +13,41 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  tabs: Tab[] = [
+    {
+      title: 'Sign up',
+      isActive: true,
+    },
+    {
+      title: 'Sign in',
+      isActive: false,
+    }
+  ];
+  selectedTab: Tab = this.tabs[0];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
   onSubmit() {
     console.log('submit')
+  }
+
+  onSelectTab(tab: Tab) {
+    this.tabs.map(t => {
+      if (tab.title === t.title) {
+        t.isActive = true;
+      } else {
+        t.isActive = false;
+      }
+    })
+    this.selectedTab = this.tabs.filter(t => tab.title === t.title)[0];
   }
 
 }
